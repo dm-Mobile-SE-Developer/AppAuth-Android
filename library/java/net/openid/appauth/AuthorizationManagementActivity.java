2a66e2e88c02ca5eb1daf3ac876b3cfe09087b92
Steffen Mayer <steffen.mayer@dm.de>
2020-01-17 16:49:14 +0100

    fix: block additional onResume if activity has not been stopped

--- library/java/net/openid/appauth/AuthorizationManagementActivity.java
+++ library/java/net/openid/appauth/AuthorizationManagementActivity.java
@@ -139,6 +139,7 @@ public class AuthorizationManagementActivity extends Activity {
     @VisibleForTesting
     static final String KEY_AUTHORIZATION_STARTED = "authStarted";
 
+    private boolean blockOnResume = false;
     private boolean mAuthorizationStarted = false;
     private Intent mAuthIntent;
     private AuthorizationManagementRequest mAuthRequest;
@@ -210,6 +211,12 @@ public class AuthorizationManagementActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        // Block additional onResume if activity has not been stopped
+        if (blockOnResume) {
+            return;
+        } else {
+            blockOnResume = true;
+        }
 
         /*
          * If this is the first run of the activity, start the authorization intent.
@@ -258,6 +265,12 @@ public class AuthorizationManagementActivity extends Activity {
         outState.putString(KEY_AUTH_REQUEST, mAuthRequest.jsonSerializeString());
         outState.putParcelable(KEY_COMPLETE_INTENT, mCompleteIntent);
         outState.putParcelable(KEY_CANCEL_INTENT, mCancelIntent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        blockOnResume = false;
     }
 
     private void handleAuthorizationComplete() {
